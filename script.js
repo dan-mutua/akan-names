8c8d3b6951bc6420547c70f461f38bfc523671bf
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-07-18 23:30:55 +0300

    add js validate function

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
+function validate() {
+  var date = document.ggetElementsByClassName("date").value;
+  console.log(date);
+  var month = document.getElementsByClassName("month").value;
+  console.log(month);
+  var year = document.getElementsByClassName("year").value;
+  console.log(year);
+  var year = document.getElementsByClassName("gender").value;
+  console.log(gender);
+}
 
-const male = [ "Kwame","Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
-                 
-const female = [ "Ama","Akosua","Adwoa", "Abenaa","Akua", "Yaa", "Afua"];
+  if (date == " " || month == " " || year == " " || gender == " ") {
+      return false;
+  } else {
+      var CC = year.slice(2);
+      var YY = year.slice(-2);
+      alert("CC" + CC + "YY" + YY)
+      let fullBirthday = year + "-" + month + "-" + date;
+      let dayvalue = new Date(fullBirthday);
+      console.log(dayvalue);
+      let dayOfTheWeek = dayvalue.getDay();
 
-    
- 
-  let year = function(){
-    const yr=document.querySelector(".na").addEventListener("change",function(){
-      const year=document.querySelector(".na").textcontent; 
-      return year;
-      
-     });     
-     
-     
+      return dayOfTheWeek;
   }
-  let mw=year();
-  console.log(typeof(mw));
- 
 
+  function myAkanName() {
+    let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  }
