f74aa889b6e720a931ee8f48bae2a3a9aafc0d17
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-07-18 20:30:07 +0300

    js year calculation

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const male = [ "Kwame","Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
+                 
+const female = [ "Ama","Akosua","Adwoa", "Abenaa","Akua", "Yaa", "Afua"];
+
+    
+ 
+  let year = function(){
+    const yr=document.querySelector(".na").addEventListener("change",function(){
+      const year=document.querySelector(".na").textcontent; 
+      return year;
+      
+     });     
+     
+     
+  }
+  let mw=year();
+  console.log(typeof(mw));
+ 
+
