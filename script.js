8ee2149717106ce31e5cbc51ff04d1ab71a22d9d
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-07-19 22:58:53 +0300

    add function to js file

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 function validate() {
   let name = document.querySelector(".name").value;
+  console.log(name);
   let date = document.querySelector(".date").value;
+  console.log(date);
   let month = document.querySelector(".month").value;
+  console.log(month);
   let year = document.querySelector(".year").value;
   console.log(year);
   
-}
-
-  if  (date == "" || month == "" || year == "" || gender == "") {
-      alert("kindly input all the fields");
-  }else {
-    var CC = year.slice(2);
-     var YY = year.slice(-2);
-     let fullBirthday = year + "-" + month + "-" + date;
-     alert("CC" + CC + "YY" + YY)
-     let dayvalue = new Date(fullBirthday);
-     console.log(dayvalue);
-     let day = dayvalue.getDay();
-
-     return (day);
+  let fullBirthDay = year + "-" + month + "-" + date;
+  let yearBorn = new Date(fullBirthDay);
+  let d = yearBorn.getDay(); //gets the day the user was born
+  console.log(d); // for testing
+  let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var genders = document.querySelector('input[name="gender"]:checked');
+  if(genders == null) {
+      alert("Select gender")
   }
+   function Aname() {
+    
+       console.log(genders);
+       genders.forEach(gender => {
+           if (gender.checked) {
+               console.log(You checked: ${gender.value});
+               console.log(gender.value);
+               console.log(gender);
+           }
+       });
+   }
+  if (genders.value === "female") {
+      document.querySelector(".any").innerHTML = "Your Akan name is" + female[d];
+      // document.querySelector(".text").innerHTML = "Your birth day is" + weekDays[validate()];
+      // console.log("Your Akan name is" + femaleNames[validate()]);
+  }
+  else if (genders.value === "male") {
+      document.querySelector(".any").innerHTML = "Your Akan name is" + male[d];
+      // document.querySelector(".text").innerHTML = "Your birth day is" + weekDays[validate()];
+      // console.log("Your Akan name is" + maleNames[validate()]);
+      validate();
+  }
+}
